49881211990e3e5a90a9b7b47ca1f9ba9d57f0c3
Arturo Chong Lin <arturo.chong.lin@gmail.com>
2019-10-24 09:33:52 -0400

    Renamed unzip file to a more general name and added function to move the file to the Downloads directory.

--- decompress_files_to_folder.sh
+++ decompress_files_to_folder.sh
@@ -1,7 +1,7 @@
-DOWNLOADS_PATH="/home/achlorine/Downloads"
+DOWNLOADS_PATH="/home/$(whoami)/Downloads"
 function process_files {
     for i in *.zip; do unzip "$i" -d "${i%%.zip}"; rm "$i"; done 
-    for i in *.rar; do unrar "$i" -d "${i%%.rar}"; rm "$i"; done 
+    for i in *.rar; do unrar e "$i" "${i%%.rar}"; rm "$i"; done 
 }
 
 function delete_torrents {
@@ -16,5 +16,10 @@ function setup {
     sudo pacman -Syy unzip unrar
 }
 
+function move_to_downloads {
+    mv 'decompress_files_to_folder.sh' ~/Downloads
+}
+
+move_to_downloads
 delete_torrents
 process_files
